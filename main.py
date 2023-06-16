ffae8c1d24ed4296f1f039065bbe9f18a93f347d
JhonatanC30 <136073681+JhonatanC30@users.noreply.github.com>
2023-06-16 17:47:01 +0000

    Implementacion de modulos

---
+++ main.py
@@ -0,0 +1,10 @@
+import math 
+from matematicas import sumar, restar, multilplicar, dividir
+from geometria import distancia
+
+print(math.sqrt(25))
+print("suma: ", sumar(2, 3))
+print("resta: ", restar(2, 3))
+print("multiplicacion: ", multilplicar(6, 4))
+print("dividir: ", dividir(36, 6))
+print("Distancia: ", distancia(1, 1, 3, 4))
